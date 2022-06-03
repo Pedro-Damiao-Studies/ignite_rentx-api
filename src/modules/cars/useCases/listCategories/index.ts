0e17103b9274ad7b86a3970805ba36b9138f2480
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-06-03 10:43:46 -0300

    Conhecendo Singleton Pattern

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,8 +1,8 @@
-import { CategoriesRepository } from '../../repositories/CategoriesRepositories';
+import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 import { ListCategoriesController } from './ListCategoriesController';
 import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 
