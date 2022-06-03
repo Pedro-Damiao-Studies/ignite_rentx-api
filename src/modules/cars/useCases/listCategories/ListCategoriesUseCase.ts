0b5d1c877a1493f6c1c7f54ff2fe35e67c85bd3e
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-06-03 09:53:17 -0300

    Refatorando a listagem de categoria

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,18 @@
+import { Category } from '../../model/Category';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+class ListCategoriesUseCase {
+  private categoriesRepository: ICategoriesRepository;
+
+  constructor(categoryRepository: ICategoriesRepository) {
+    this.categoriesRepository = categoryRepository;
+  }
+
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
