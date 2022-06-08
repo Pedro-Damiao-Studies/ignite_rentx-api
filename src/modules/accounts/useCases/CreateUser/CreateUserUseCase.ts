5e9af4879270a2953084a9fcc360bc354e52dba3
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-06-08 15:46:59 -0300

    Criptografrar senha

--- src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
@@ -11,7 +12,15 @@ class CreateUserUseCase {
   ) {}
 
   async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
-    await this.usersRepository.create({ name, email, password, driver_license });
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists.');
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({ name, email, password: passwordHash, driver_license });
   }
 }
 
