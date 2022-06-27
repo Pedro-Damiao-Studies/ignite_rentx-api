62ccb4ac1b8449a71c68d043bbcc2da1998136a6
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-06-27 09:22:08 -0300

    Estruturando a entidade de carros

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { authenticateRoutes } from '@shared/infra/http/routes/authenticate.routes';
+import { carsRoutes } from '@shared/infra/http/routes/cars.routes';
 import { categoriesRoutes } from '@shared/infra/http/routes/categories.routes';
 import { specificationsRoutes } from '@shared/infra/http/routes/specification.routes';
 import { usersRoutes } from '@shared/infra/http/routes/user.routes';
@@ -11,5 +12,6 @@ router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
 router.use('/sessions', authenticateRoutes);
+router.use('/cars', carsRoutes);
 
 export { router };
