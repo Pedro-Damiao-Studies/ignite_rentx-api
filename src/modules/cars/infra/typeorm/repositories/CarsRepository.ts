34f79eb691d27601a3f5092276076f1ff1e977c3
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-06-29 09:33:53 -0300

    Fix filename

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -1,7 +1,7 @@
 import { getRepository, Repository } from 'typeorm';
 
 import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
-import { Car } from '@modules/cars/infra/typeorm/entities/Cars';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 
 class CarsRepository implements ICarsRepository {
