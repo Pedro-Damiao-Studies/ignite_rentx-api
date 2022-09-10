30d5fbb6f28b92874453956f6af45bd7f66dc11a
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-09-10 19:04:46 -0300

    Recuperação de Senha

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,50 @@
+import { readFileSync } from 'fs';
+import handlebars from 'handlebars';
+import nodemailer, { Transporter } from 'nodemailer';
+import { injectable } from 'tsyringe';
+
+import { IMailProvider } from '@shared/container/providers/MailProvider/IEmailProvider';
+
+@injectable()
+class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer
+      .createTestAccount()
+      .then((account) => {
+        this.client = nodemailer.createTransport({
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+            user: account.user,
+            pass: account.pass,
+          },
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    const templateFileContent = readFileSync(path).toString('utf-8');
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      from: 'Rentx <noreply@rentx.com.br>',
+      subject,
+      html: templateHTML,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
+
+export { EtherealMailProvider };
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,5 +2,9 @@ import { container } from 'tsyringe';
 
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { DayJsDateProvider } from '@shared/container/providers/DateProvider/implementations/DayJsDateProvider';
+import { IMailProvider } from '@shared/container/providers/MailProvider/IEmailProvider';
+import { EtherealMailProvider } from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+
+container.registerSingleton<IMailProvider>('MailProvider', EtherealMailProvider);
 
 container.registerSingleton<IDateProvider>('DateProvider', DayJsDateProvider);
